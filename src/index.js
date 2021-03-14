27e273ce6deb45a1b22ef893ed103c57fdca7e9f
YurBat <47013577+YurBat@users.noreply.github.com>
2021-03-14 05:05:18 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,32 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	if (!matrix){
+		return [];
 }
+	let matrixLenght = matrix.length
+	for (i = 0; i < matrixLenght; i++) {
+		if (i % 2 != 0) matrix[i].reverse();
+		}
+	
+	return matrix.flat(Infinity);
+
+}
+
+
+/*
+let a = [matrix]; 
+let b = a.flat(Infinity);
+return b;
+console.log(b)
+*/
+
+1. /*определяем кол. подмассивов
+matrix.length;
+*/
+2. /*каждый второй переворачиваем
+even.reverse();
+*/
+3. /*сводим всё в один массив
+concat() flat
+*/
